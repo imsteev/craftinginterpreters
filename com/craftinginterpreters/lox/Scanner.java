8a7c3cc2de27f17dbc0faecf17e441958586f39e
imsteev <stephen@withclad.com>
2025-12-21 11:14:31 -0500

    end ch. 4

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -10,9 +10,37 @@ import java.util.Map;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+
+    // Start of a lexeme.
     private int start = 0;
+
+    // Current position in the line.
     private int current = 0;
+
+    // Line number in the source.
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",       AND);
+        keywords.put("class",   CLASS);
+        keywords.put("else",     ELSE);
+        keywords.put("false",   FALSE);
+        keywords.put("for",       FOR);
+        keywords.put("fun",       FUN);
+        keywords.put("if",         IF);
+        keywords.put("nil",       NIL);
+        keywords.put("or",         OR);
+        keywords.put("print",   PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",   SUPER);
+        keywords.put("this",     THIS);
+        keywords.put("true",     TRUE);
+        keywords.put("var",       VAR);
+        keywords.put("while",   WHILE);
+    }
     
     Scanner(String source) {
         this.source = source;
@@ -70,10 +98,48 @@ public class Scanner {
                 line++;
                 break;
             case '"': string(); break;
+
+            // Maximal munch
+            //   When two lexical grammar rules can both match a chunk of code, whichever one
+            //   matches the most characters wins.
+            case 'o':
+                if (match('r')) {
+                    addToken(OR);
+                }
+                break;
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -106,6 +172,24 @@ public class Scanner {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
 
     private boolean isAtEnd() {
         return current >= source.length();

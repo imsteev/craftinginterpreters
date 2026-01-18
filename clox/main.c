4bd27a692d00fabfa308e2386ba56f545cec3a8a
imsteev <stephen@withclad.com>
2026-01-18 15:15:30 -0500

    scaffold vm run

--- clox/main.c
+++ clox/main.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 // clox
 // front-end: compiler
@@ -8,6 +9,8 @@
 // execution: virtual machine (VM)
 
 int main(int argc, const char *argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
 
@@ -18,6 +21,8 @@ int main(int argc, const char *argv[]) {
   writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }

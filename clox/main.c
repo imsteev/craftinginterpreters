a03851e19399fb2b1ee07af62a2794f6787dcf9a
imsteev <stephen@withclad.com>
2026-01-18 07:42:45 -0500

    fix ci

--- clox/main.c
+++ clox/main.c
@@ -11,6 +11,8 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   writeChunk(&chunk, OP_RETURN);
+
+  disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
   return 0;
 }
